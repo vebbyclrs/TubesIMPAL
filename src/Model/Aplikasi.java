36945f630d55746f8af69c6099bf304b8bfaf3a8
endriawan01 <endriawan.03@gmail.com>
2017-11-28 20:45:36 +0700

    nambahin loadjadwal,getJadwal, sama di contMahasiswa Rubah Vmahasiswa,databasenya tambahanan beberapa method kyk getjadwal, savejadwal,loadjadwal aplikasi juga savejadwal

--- src/Model/Aplikasi.java
+++ src/Model/Aplikasi.java
@@ -29,7 +29,7 @@ public class Aplikasi {
         daftarDosen = loadDosen();
         daftarMahasiswa = loadMahasiswa();
         daftarMatkul = loadMatkul();
-        System.out.println(daftarMahasiswa.get(daftarMahasiswa.size() - 1));
+        
 
         //load semua array
     }
@@ -257,6 +257,7 @@ public class Aplikasi {
         daftarJadwal = new ArrayList<Jadwal>();
         try {
             for (MataKuliah matakuliah : daftarMatkul) {
+                
                 String query = "select * from jadwal where id_matkul=" + matakuliah.getKodeMk() + ";";
                 ResultSet rs = db.getData(query);
 
@@ -284,12 +285,16 @@ public class Aplikasi {
                 if (jadwal.getMatkul().getKodeMk() == id_matkul){
                     jdwl = jadwal;
                 }
+                jadwal.getIdJadwal();
                 
             }
         } catch (Exception e) {
             throw new IllegalArgumentException("gagal getJadwal");
         }
+        
         return jdwl;
     }
-
+    public void saveJadwalTaken(ArrayList<Jadwal> jadwal, long nim){
+        System.out.println(2222);
+    }
 }
